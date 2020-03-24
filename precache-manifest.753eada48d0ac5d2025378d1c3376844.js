0dec0b6b88ce4f8980751085a6bac1c93c81f960
Mateusz Popławski <mateusz.poplawski.1993@gmail.com>
2020-03-24 12:58:07 +0100

    Updates

---
+++ precache-manifest.753eada48d0ac5d2025378d1c3376844.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8085c84201963abb1dd05aba20677b87",
+    "url": "/skills-matrix/index.html"
+  },
+  {
+    "revision": "f1f99c53b55ba05c75c0",
+    "url": "/skills-matrix/static/css/main.788bf44a.chunk.css"
+  },
+  {
+    "revision": "ec33734ccb800517ef11",
+    "url": "/skills-matrix/static/js/2.c19e5289.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/skills-matrix/static/js/2.c19e5289.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1f99c53b55ba05c75c0",
+    "url": "/skills-matrix/static/js/main.b9ff4e48.chunk.js"
+  },
+  {
+    "revision": "c0669aeee73d19ed211c",
+    "url": "/skills-matrix/static/js/runtime-main.bedc98b4.js"
+  }
+]);
